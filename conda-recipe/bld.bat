ad5ad238a7683f78b6dc03c0076a3553e92dcba3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-15 02:15:05 +0000

    Deploy Fluorescence-Tools/fit2x to github.com/Fluorescence-Tools/fit2x.git:gh-pages

--- conda-recipe/bld.bat
+++
@@ -1,2 +0,0 @@
-rmdir build /s /q
-"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
--- conda-recipe/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-rm -r -f build
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
