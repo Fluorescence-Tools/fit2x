f8300dcefc4aa918d3df9dfa3ef98f8b9cf8c32a
Thomas-Otavio Peulen <thomas.peulen@posteo.de>
2021-04-13 00:26:49 -0700

    Find broken test

--- include/Decay.h
+++ include/Decay.h
@@ -98,13 +98,13 @@ public:
             std::string score_type= "poisson"
     );
 
-//    ~Decay(){
-//        delete decayPileup;
-//        delete decayScale;
-//        delete decayLinearization;
-//        delete decayScore;
-//        delete decayLinearization;
-//    }
+    ~Decay(){
+        delete decayPileup;
+        delete decayScale;
+        delete decayLinearization;
+        delete decayScore;
+        delete decayLinearization;
+    }
 
     bool get_is_valid() const {
         return _is_valid;
