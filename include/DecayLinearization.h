a5ae088e283a5294356066a782163b04d61b60bf
Thomas-Otavio Peulen <thomas.peulen@posteo.de>
2021-04-13 14:09:16 -0700

    Find broken test

--- include/DecayLinearization.h
+++ include/DecayLinearization.h
@@ -13,11 +13,12 @@ class DecayLinearization: public DecayModifier{
     friend class DecayCurve;
 
 private:
-    /// The model function is multiplied by this vector is _use_linearization is true
-    std::vector<double> _linearization_table = std::vector<double>();
 
     /// If set to true multiply the linearization to the model function
-    int _use_linearization = false;
+    bool _use_linearization;
+
+    /// The model function is multiplied by this vector is _use_linearization is true
+    std::vector<double> _linearization_table;
 
 public:
 
@@ -69,16 +70,21 @@ public:
         return _use_linearization;
     }
 
-    DecayLinearization(
-            double* linearization_table, int n_linearization_table,
-            bool use_linearization = false){
+
+    DecayLinearization() : _use_linearization(false){
 #ifdef VERBOSE_FIT2X
         std::clog << "DecayLinearization::DecayLinearization" << std::endl;
 #endif
+    }
+
+    DecayLinearization(
+            double* linearization_table, int n_linearization_table,
+            bool use_linearization = false) : DecayLinearization(){
+#ifdef VERBOSE_FIT2X
+        std::clog << "DecayLinearization::DecayLinearization(double*, int, bool)" << std::endl;
+#endif
         set_linearization_table(linearization_table, n_linearization_table);
-        // TODO: Check why setting _use_linearization causes a segfault
         set_use_linearization(use_linearization);
-        // _use_linearization = use_linearization;
     }
 
     void add(DecayCurve* decay){
