dea2a55851910691b047c04a98c5527158d6c7ea
tpeulen <thomas.otavio.peulen@gmail.com>
2021-04-26 20:37:32 -0700

    Fixed example

--- examples/decay/plot_decay_analysis_1.py
+++ examples/decay/plot_decay_analysis_1.py
@@ -38,7 +38,7 @@ Create an instance the ``Decay`` class
 
 .. code-block::python
 
-    decay_object = fit2x.Decay(
+    decay = fit2x.Decay(
         data=data_decay.astype(np.float64),
         instrument_response_function=irf.astype(np.float64),
         time_axis=time_axis,
@@ -59,60 +59,36 @@ import fit2x
 
 def objective_function_chi2(
         x: np.ndarray,
-        decay_object: fit2x.Decay,
+        decay: fit2x.Decay,
         x_min: int = 20,
         x_max: int = 150
 ):
     scatter, background, time_shift, irf_background = x[0:4]
-    lifetime_spectrum = x[4:]
-    decay_object.set_lifetime_spectrum(lifetime_spectrum)
-    decay_object.irf_background_counts = irf_background
-    decay_object.scatter_fraction = scatter
-    decay_object.constant_offset = background
-    decay_object.irf_shift = time_shift
-    # wres = decay_object.get_weighted_residuals()
+    lifetime_spectrum = np.abs(x[4:])
+    decay.set_lifetime_spectrum(lifetime_spectrum)
+    decay.irf_background_counts = irf_background
+    decay.scatter_fraction = scatter
+    decay.constant_offset = background
+    decay.irf_shift = time_shift
+    # wres = decay.get_weighted_residuals()
     # return np.sum(wres[x_min:x_max]**2)
-    return decay_object.get_score(x_min, x_max)
+    return decay.get_score(x_min, x_max)
 
 
 def objective_function_mle(
         x: np.ndarray,
-        x0: np.ndarray,
-        decay_object: fit2x.Decay,
+        decay: fit2x.Decay,
         x_min: int = 20,
-        x_max: int = 500,
-        use_amplitude_prior: bool = True,
-        use_initial_prior: bool = True,
-        amplitude_bias: float = 5.0,
-        initial_sd: float = 5.0,
-        verbose: bool = False
+        x_max: int = 500
 ):
     scatter, background, time_shift, irf_background = x[0:4]
     lifetime_spectrum = np.abs(x[4:])
-    decay_object.set_lifetime_spectrum(lifetime_spectrum)
-    decay_object.irf_background_counts = irf_background
-    decay_object.scatter_fraction = scatter
-    decay_object.constant_offset = background
-    decay_object.irf_shift = time_shift
-    chi2_mle = decay_object.get_score(x_min, x_max, score_type="poisson")
-    # d = decay_object.get_data()[x_min:x_max]
-    # m = decay_object.get_model()[x_min:x_max]
-    # return np.sum((m - d) - d * np.log(m/d))
-    ap = 0.0
-    if use_amplitude_prior:
-        p = lifetime_spectrum[::2]
-        a = np.ones_like(p) + amplitude_bias
-        ap = scipy.stats.dirichlet.logpdf(p / np.sum(p), a)
-        chi2_mle += ap
-    ip = 0.0
-    if use_initial_prior:
-        ip = -np.sum(np.log(1./((1+((x - x0) / initial_sd)**2.0))))
-        chi2_mle += ip
-    if verbose:
-        print("Total log prob: %s" % chi2_mle)
-        print("Parameter log prior: %s" % ip)
-        print("Dirichlet amplitude log pdf: %s" % ap)
-        print("---")
+    decay.set_lifetime_spectrum(lifetime_spectrum)
+    decay.irf_background_counts = irf_background
+    decay.scatter_fraction = scatter
+    decay.constant_offset = background
+    decay.irf_shift = time_shift
+    chi2_mle = decay.get_score(x_min, x_max, score_type="poisson")
     return chi2_mle
 
 
@@ -126,10 +102,10 @@ data = tttrlib.TTTR(spc132_filename, 'SPC-130')
 ch0_indeces = data.get_selection_by_channel([0, 8])
 data_ch0 = data[ch0_indeces]
 
-n_bins = 512
+n_bins = 128
 # selection from tttr object
 cr_selection = data_ch0.get_selection_by_count_rate(
-    time_window=6.0, n_ph_max=7
+    time_window=6.0e-3, n_ph_max=7
 )
 low_count_selection = data_ch0[cr_selection]
 # create histogram for IRF
@@ -139,41 +115,44 @@ irf, _ = np.histogram(low_count_selection.micro_times, bins=n_bins)
 # equivalent selection using selection function
 cr_selection = tttrlib.selection_by_count_rate(
     data_ch0.macro_times,
-    0.100, n_ph_max=5,
-    macro_time_calibration=data.header.macro_time_resolution / 1e6,
+    0.100e-3, n_ph_max=5,
+    macro_time_calibration=data.header.macro_time_resolution,
     invert=True
 )
 high_count_selection = data_ch0[cr_selection]
 data_decay, _ = np.histogram(high_count_selection.micro_times, bins=n_bins)
-time_axis = np.arange(0, n_bins) * data.header.micro_time_resolution * 4096 / n_bins
+time_axis = np.arange(0, n_bins) * data.header.micro_time_resolution * 4096 * 1e9 / n_bins  # in [ns]
 
 ###################################
 #     Make decay object
 ###################################
-decay_object = fit2x.Decay(
+decay = fit2x.Decay(
     data=data_decay.astype(np.float64),
     irf_histogram=irf.astype(np.float64),
     time_axis=time_axis,
-    excitation_period=data.header.macro_time_resolution,
-    lifetime_spectrum=[1., 1.2, 1., 3.5]
+    scale_model_to_data=True,
+    excitation_period=max(time_axis),
+    lifetime_spectrum=[1., 1.2, 1., 3.5],
+    abs_lifetime_spectrum=True,
+    convolution_method=fit2x.ConvFastPeriodicTime
 )
-decay_object.evaluate()
+decay.update_model()
 
 # A minimum number of photons should be in each channel
 # as no MLE is used and Gaussian distributed errors are assumed
 sel = np.where(data_decay > 1)[0]
-x_min = 10 #int(min(sel))
+x_min = 2 #int(min(sel))
 
 # The BH card are not linear at the end of the TAC. Thus
 # fit not the full range
-x_max = 450 #max(sel)
+x_max = 110 #max(sel)
 
 # Set some initial values for the fit
 scatter = [0.05]
-background = [2.01]
-time_shift = [2]
+background = [0.01]
+time_shift = [0]
 irf_background = [5]
-lifetime_spectrum = [0.5, 0.5, 0.5, 3.5]
+lifetime_spectrum = [0.5, 0.5, 0.5, 2.5]
 
 ###################################
 #     Optimize                    #
@@ -187,16 +166,12 @@ x0 = np.hstack(
         lifetime_spectrum
     ]
 )
-fit = scipy.optimize.minimize(
-    objective_function_mle, x0,
-    args=(x0, decay_object, x_min, x_max, False, False)
-)
-fit_mle = fit.x
+
 
 x0 = np.hstack([scatter, background, time_shift, irf_background, lifetime_spectrum])
 fit = scipy.optimize.minimize(
-    objective_function_mle, x0,
-    args=(x0, decay_object, x_min, x_max)
+    objective_function_chi2, x0,
+    args=(decay, x_min, x_max)
 )
 fit_map = fit.x
 
@@ -204,16 +179,14 @@ fit_map = fit.x
 #     Plot                        #
 ###################################
 fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, sharey=False)
-ax[1].semilogy(time_axis, irf, label="IRF")
-ax[1].semilogy(time_axis, data_decay, label="Data")
-ax[1].semilogy(
-    time_axis[x_min:x_max],
-    decay_object.model[x_min:x_max], label="Model"
-)
+ax[1].semilogy(decay.time_axis, decay.corrected_irf, label="IRF")
+ax[1].semilogy(decay.time_axis, decay.data, label="Data")
+ax[1].semilogy(decay.time_axis[x_min:x_max], decay.model[x_min:x_max], label="Model")
+
 ax[1].legend()
 ax[0].plot(
     time_axis[x_min:x_max],
-    decay_object.weighted_residuals[x_min:x_max],
+    decay.weighted_residuals[x_min:x_max],
     label='w.res.',
     color='green'
 )
