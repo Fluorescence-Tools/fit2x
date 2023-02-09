0074d82068d7715661f01019f7d72e70caca851e
tpeulen <thomas.peulen@posteo.de>
2023-02-09 18:14:21 +0100

    Remove AVX on apple

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,11 +1,15 @@
-rmdir build /s /q
+REM rmdir build /s /q
 git submodule update --recursive --init --remote
-%PYTHON% setup.py install --single-version-externally-managed --record=record.txt
+REM %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
+md build
 cd build
 cmake .. -G "NMake Makefiles" ^
-  -DCMAKE_INSTALL_PREFIX=%PREFIX%/Library ^
-  -DCMAKE_BUILD_TYPE=Release ^
-  -DBUILD_PYTHON_INTERFACE=OFF ^
-  -DCMAKE_PREFIX_PATH=%PREFIX%
+ -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+ -DCMAKE_PREFIX_PATH=%PREFIX% ^
+ -DBUILD_PYTHON_INTERFACE=ON ^
+ -DCMAKE_BUILD_TYPE=Release ^
+ -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=%SP_DIR% ^
+ -DCMAKE_SWIG_OUTDIR=$SP_DIR
+
 nmake
 nmake install
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,14 +1,17 @@
-#!/usr/bin/env bash
 git submodule update --recursive --init --remote
+
 rm -r -f build
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+#$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+mkdir build
 cd build
 
 cmake \
  -DCMAKE_INSTALL_PREFIX=$PREFIX \
  -DCMAKE_PREFIX_PATH=$PREFIX \
- -DBUILD_PYTHON_INTERFACE=OFF \
+ -DBUILD_PYTHON_INTERFACE=ON \
  -DCMAKE_BUILD_TYPE=Release \
+ -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$SP_DIR \
+ -DCMAKE_SWIG_OUTDIR=$SP_DIR \
  ..
 
 make
